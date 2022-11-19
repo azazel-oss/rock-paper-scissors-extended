96b991a2cba73ee4e89725c77e2a8ad84de4e1cf
Asad Mahmood <asadmhmd1234@gmail.com>
2022-11-20 02:06:41 +0530

    Add rules modal to the app

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import rules from "../assets/images/image-rules-bonus.svg";
+import close from "../assets/images/icon-close.svg";
 import "./Rules.css";
 
-function Rules(props) {
+function Rules() {
+  const [isHidden, setIsHidden] = useState(true);
   return (
-    <footer>
-      <button className={"btn-rules"}>Rules</button>
-    </footer>
+    <div className={"rules"}>
+      <button className={"btn-rules"} onClick={() => setIsHidden(false)}>
+        Rules
+      </button>
+      <div className={`modal ${isHidden ? "hidden" : ""}`}>
+        <div>Rules</div>
+        <div className={"rules-img-container"}>
+          <img src={rules} alt={"rules"} />
+        </div>
+        <div className={"close-icon-container"}>
+          <span onClick={() => setIsHidden(true)}>
+            <img src={close} alt={"close icon"} />
+          </span>
+        </div>
+      </div>
+    </div>
   );
 }
 
