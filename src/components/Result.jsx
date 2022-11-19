1f6ed90215dc8804c18e2e9cd75325339a242739
Asad Mahmood <asadmhmd1234@gmail.com>
2022-11-19 17:40:20 +0530

    Add styles and structure to the result page

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from "react";
+import rock from "../assets/images/icon-rock.svg";
+import paper from "../assets/images/icon-paper.svg";
+import scissors from "../assets/images/icon-scissors.svg";
+import spock from "../assets/images/icon-spock.svg";
+import lizard from "../assets/images/icon-lizard.svg";
+import "./Result.css";
 
 function Result(props) {
   const [computerChoice, setComputerChoice] = useState("");
   const [result, setResult] = useState("");
   const choices = ["rock", "paper", "scissors", "spock", "lizard"];
+  const icons = {
+    rock: rock,
+    paper: paper,
+    scissors: scissors,
+    spock: spock,
+    lizard: lizard,
+  };
   useEffect(() => {
     let timer = setTimeout(() => {
       const randomChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -46,13 +59,31 @@ function Result(props) {
 
   return (
     <div>
-      <div>You chose {props.choice}</div>
-      <div>Computer chose {computerChoice}</div>
+      <div className={"choices"}>
+        <div className={"choice__result"}>
+          <div className={`element element-${props.choice}`}>
+            <img src={icons[props.choice]} alt={`${props.choice} icon`} />
+          </div>
+          <div>You picked</div>
+        </div>
+        <div className={"choice__result"}>
+          {computerChoice ? (
+            <div className={`element element-${computerChoice}`}>
+              <img src={icons[computerChoice]} alt={`${computerChoice} icon`} />
+            </div>
+          ) : (
+            <div className={"blank-icon"}></div>
+          )}
+          <div>The bot picked</div>
+        </div>
+      </div>
       {computerChoice && result && (
-        <>
-          <div>You {result}</div>
-          <div onClick={props.onGameReset}>Play again?</div>
-        </>
+        <div className={"result"}>
+          <div className={"result__text"}>You {result}</div>
+          <div className={"btn-reset"} onClick={props.onGameReset}>
+            Play again?
+          </div>
+        </div>
       )}
     </div>
   );
