e2913eb0975604df2e7d33437cb049cc6183ed01
Asad Mahmood <asadmhmd1234@gmail.com>
2022-11-19 17:38:04 +0530

    Add rules button and style it

---
+++ src/components/Rules.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Rules.css";
+
+function Rules(props) {
+  return (
+    <footer>
+      <button className={"btn-rules"}>Rules</button>
+    </footer>
+  );
+}
+
+export default Rules;
